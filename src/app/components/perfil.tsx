5ced2b628e6e1264e8ff022845f43111259609d5
Giselle jacinto <gisellejacinto1219@gmail.com>
2023-12-12 22:27:31 -0300

    Adicionado componente do perfil

---
+++ src/app/components/perfil.tsx
@@ -0,0 +1,13 @@
+import { User } from '@nextui-org/react';
+
+export default function Perfil() {
+  return (
+    <User
+      name="Jane Doe"
+      description="Product Designer"
+      avatarProps={{
+        src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
+      }}
+    />
+  );
+}
