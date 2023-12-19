102167240bb9acbc799a31d90fada68e76759af8
MariCruz <maricruzdev@gmail.com>
2023-12-19 14:39:27 -0300

    update header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,15 @@
 export default function Header() {
   return (
-    <header>
-      <h1>Timesync</h1>
+    <header className="flex bg-lime-900 justify-between items-center py-2 px-10">
+      {/* logo da softex e input - direita, buscar evento */}
+      <a href="/">
+        <img
+          src="https://fap.softexrecife.org.br/assets/logo_softex_branca-8d8eb306ecdd1540cc4df965b41a811b428ef7fc9beec550d16902884ff9d117.png"
+          alt="logo"
+        />
+      </a>
+
+      <input className="bg-white rounded-xl " type="text" />
     </header>
   );
 }
