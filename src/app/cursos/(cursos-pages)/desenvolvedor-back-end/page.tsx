7deb6c23af079e14262a578e724d14a58ea436bf
sthallysson <sthallysson5@gmail.com>
2024-01-03 16:10:31 -0300

    feat: add courses page with your components

---
+++ src/app/cursos/(cursos-pages)/desenvolvedor-back-end/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function BackEnd() {
+  return <div>Backend</div>;
+}
---
+++ src/app/cursos/(cursos-pages)/desenvolvedor-front-end/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FrontEnd() {
+  return <div>FrontEnd</div>;
+}
