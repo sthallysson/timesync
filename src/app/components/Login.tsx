b1d9b45e1da56903ec33a5b907d6c925b1cbfdfa
Gabriel P. Santos <gabrielp_santos@hotmail.com>
2024-01-04 16:15:31 -0300

    feat: add component Login

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -3,42 +3,99 @@
 
 import { Input, Button } from "@nextui-org/react";
 import {useState} from 'react';
+import {useForm} from 'react-hook-form';
 import { MdLockOutline, MdLockOpen } from "react-icons/md";
+import Lottie from 'react-lottie';
+
+import * as calendarAnimation from '../../../public/json/calendar.json';
+
+import {zodResolver} from '@hookform/resolvers/zod'
+import {z} from 'zod';
+
+type createUserFormData = z.infer<typeof createUserFormSchema>
+
+// Representação da estrutura de dados e suas limitações
+const createUserFormSchema = z.object({
+    email: z.string().min(0, "O email não pode estar vazio").email("Formato de e-mail invalido").toLowerCase(),
+    password: z.string().min(6, "A senha precisa de no minimo 6 caracteres")
+})
 
 export default function Login(){
+    const [output, setOutput] = useState('')
     const [isVisible, setIsVisible] = useState(false);
+    
+    const {register, handleSubmit, formState: {errors}} = useForm<createUserFormData>({
+        resolver: zodResolver(createUserFormSchema),
+    });
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: calendarAnimation,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    }
 
     const toggleVisibility = () =>{setIsVisible(!isVisible);};
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function createUser(data: any){
+        setOutput(JSON.stringify(data, null, 2));
+        console.log(output);
+    }
+
     return (
         <main
-        className=" flex items-center justify-center w-screen h-screen text-lg bg-green-600"
+        className=" flex items-center justify-between w-screen h-screen text-lg"
         >
-            <div 
-            className="w-2/6 h-3/5 p-3 text-center flex flex-col justify-evenly border-3 border-black
-            rounded-md bg-white"
+            <div className="m-16">
+                <Lottie options={defaultOptions}
+                height={700}
+                width={700}
+                
+                />
+            </div>
+
+            <form 
+            onSubmit={handleSubmit(createUser)}
+            className="w-4/12 h-3/5 p-3 text-center flex flex-col justify-evenly"
             >
-                <h2 className="mb-14 underline">Tela Login</h2>
-                <div className="">
+                <h1 className="text-4xl">Login</h1>
+                <div className="p-8">
                     <Input
-                    className="my-5"
+                    className="my-6"
                     type='email' 
-                    label='Email' 
+                    label='Email'
+                    isInvalid={errors.email}
+                    errorMessage={errors.email?.message}
+                    color={errors.email ? "danger" : "primary"}
+                    {...register('email')}
                     />
 
                     <Input
-                    className="my-5"
+                    className="my-6"
                     type= {isVisible? 'text':'password'} 
                     label='Senha'
+                    isInvalid={errors.password}
+                    errorMessage={errors.password?.message}
+                    color={errors.password ? "danger" : "primary"}
                     endContent={
                         <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
                             {isVisible ? (<MdLockOpen />) : (<MdLockOutline />)}
                         </button>
-                    }/>
+                    }
+                    {...register('password')}
+                    />
 
-                    <Button>Entrar</Button>
+                    <Button 
+                    type="submit" 
+                    color="success"
+                    className="w-full"
+                    >Entrar
+                    </Button>
                 </div>
-            </div>
+            </form>
         </main>
     );
 }
