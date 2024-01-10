f88cbe64b043f16b8e91b003333933e540eba80a
Gabriel P. Santos <gabrielp_santos@hotmail.com>
2024-01-09 23:15:11 -0300

    add: the Button changes the color when on different routes

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { Input, Button } from '@nextui-org/react';
+import NextLink from 'next/link';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { MdLockOutline, MdLockOpen } from 'react-icons/md';
